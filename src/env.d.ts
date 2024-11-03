1eca348cc54447292fadaca97fa15ab8a74062f9
EvgenyWas <evgeniywas@gmail.com>
2024-11-03 17:59:15 +0000

    feat: set up swagger

---
+++ src/env.d.ts
@@ -0,0 +1,16 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: 'development' | 'production';
+      PWD: string;
+      PORT: string;
+      CRYPT_SALT: string;
+      JWT_SECRET_KEY: string;
+      JWT_SECRET_REFRESH_KEY: string;
+      TOKEN_EXPIRE_TIME: string;
+      TOKEN_REFRESH_EXPIRE_TIME: string;
+    }
+  }
+}
+
+export {};
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import 'dotenv/config';
+
 import { AppModule } from './app.module';
+
+const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
+
+  // Setup swagger
+  const config = new DocumentBuilder()
+    .setTitle('Home Library Service')
+    .setDescription(
+      'Home Library Service! Users can create, read, update, delete data about Artists, Tracks and Albums, add them to Favorites in their own Home Library!',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+  await app.listen(PORT);
 }
 bootstrap();
