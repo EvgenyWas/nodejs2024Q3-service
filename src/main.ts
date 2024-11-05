fda24d99ba972912a1fd5ddad78846bc1030a39e
EvgenyWas <evgeniywas@gmail.com>
2024-11-05 18:46:35 +0000

    fix: remove api global path prefix

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,6 @@ const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
 
   // Setup swagger
